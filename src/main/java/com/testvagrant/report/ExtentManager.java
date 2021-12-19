9813b8ffa033733b54f4de29ecf72c998b733155
Deepak Kumar Rai <Deepakggsipu@gmail.com>
2021-12-19 21:24:34 +0530

    extent report library added and relavant classes added for reporting

---
+++ src/main/java/com/testvagrant/report/ExtentManager.java
@@ -0,0 +1,95 @@
+package com.testvagrant.report;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+import org.openqa.selenium.Platform;
+
+import java.io.File;
+import java.util.Date;
+
+public class ExtentManager {
+
+    private static ExtentReports extentReport;
+    private static Platform platform;
+    private static final Date date = new Date();
+    private static final String reportFileName =
+            "Extent_" + date.toString().replaceAll(":", "_") + ".html";
+    private static final String windowsPath = System.getProperty("user.dir") + "/test-output";
+    private static final String linuxPath = System.getProperty("user.dir") + "/test-output";
+    private static final String macPath = System.getProperty("user.dir") + "/TestReport";
+    private static final String winReportFileLoc = windowsPath + "/" + reportFileName;
+    private static final String linuxReportFileLoc = linuxPath + "/" + reportFileName;
+    private static final String macReportFileLoc = macPath + "/" + reportFileName;
+
+    public static ExtentReports getInstance() {
+        if (extentReport == null) {
+            createInstance();
+        }
+        return extentReport;
+    } // End of method getInstance
+
+    public static ExtentReports createInstance() {
+        new ExtentManager();
+        platform = getCurrentPlatform();
+        String fileName = getReportFileLocation(platform);
+        ExtentSparkReporter htmlReporter = new ExtentSparkReporter(fileName);
+        htmlReporter.config().setTheme(Theme.STANDARD);
+        htmlReporter.config().setDocumentTitle(fileName);
+        htmlReporter.config().setEncoding("utf-8");
+        htmlReporter.config().setReportName("Sample Extent Report");
+
+        extentReport = new ExtentReports();
+        extentReport.attachReporter(htmlReporter);
+
+        extentReport.setSystemInfo("Automation Tester", "Deepak Rai");
+        extentReport.setSystemInfo("Organization", "Deepak Automation Labs");
+        extentReport.setSystemInfo("Build Number", "Deepak 1.0");
+
+        return extentReport;
+    } // End of method createInstance
+
+    // Select the extent report file location based on platform
+    private static String getReportFileLocation(Platform platform) {
+        String reportFileLocation = null;
+        switch (platform) {
+            case MAC -> {
+                reportFileLocation = macReportFileLoc;
+                createReportPath(macPath);
+            }
+            case WIN10 -> {
+                reportFileLocation = winReportFileLoc;
+                createReportPath(windowsPath);
+            }
+            case LINUX -> {
+                reportFileLocation = linuxReportFileLoc;
+                createReportPath(linuxPath);
+            }
+            default -> System.out.println("ExtentReport path has not been set! There is a problem!\n");
+        }
+        return reportFileLocation;
+    } // End of method getReportFileLocation
+
+    // Create the report path if it does not exist
+    private static void createReportPath(String path) {
+        File testDirectory = new File(path);
+        if (!testDirectory.exists()) {
+            testDirectory.mkdir() ;
+        }
+    } // End of method createReportPath
+
+    // Get current platform
+    private static Platform getCurrentPlatform() {
+        if (platform == null) {
+            String OS = System.getProperty("os.name").toLowerCase();
+            if (OS.contains("win")) {
+                platform = Platform.WIN10;
+            } else if (OS.contains("nix") || OS.contains("nux") || OS.contains("aix")) {
+                platform = Platform.LINUX;
+            } else if (OS.contains("mac")) {
+                platform = Platform.MAC;
+            }
+        }
+        return platform;
+    } // End of method getCurrentPlatform
+} // End of class ExtentManager
---
+++ src/main/java/com/testvagrant/report/ExtentReportListener.java
@@ -0,0 +1,106 @@
+package com.testvagrant.report;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.Markup;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
+
+import com.testvagrant.base.BaseWebDriver;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import java.util.Objects;
+
+public class ExtentReportListener extends BaseWebDriver implements ITestListener {
+
+  // Extent Report Declarations
+  private ExtentReports extent = ExtentManager.createInstance();
+  private static ThreadLocal<ExtentTest> extentTestThreadLocal = new ThreadLocal<>();
+
+  public static synchronized ExtentTest getExtentTest() {
+    try {
+      return extentTestThreadLocal.get();
+    } catch (Exception e) {
+      return null;
+    }
+  }
+
+  @Override
+  public void onTestStart(ITestResult result) {
+    ExtentTest extentTest =
+        extent.createTest(result.getMethod().getMethodName(), result.getMethod().getDescription());
+    extentTestThreadLocal.set(extentTest);
+  }
+
+  @Override
+  public void onTestSuccess(ITestResult result) {
+    extentTestThreadLocal.get().pass("Test Case Passed");
+  }
+
+  @Override
+  public void onTestFailure(ITestResult result) {
+    Object testClass = result.getInstance();
+    WebDriver driver = ((BaseWebDriver) testClass).getDriver();
+
+    String base64Screenshot =
+        "data:image/png;base64,"
+            + ((TakesScreenshot) Objects.requireNonNull(driver)).getScreenshotAs(OutputType.BASE64);
+    // ExtentReports log and screenshot operations for failed tests.
+    extentTestThreadLocal
+        .get()
+        .log(
+            Status.FAIL,
+            "Test Failed",
+            extentTestThreadLocal
+                .get()
+                .addScreenCaptureFromBase64String(base64Screenshot)
+                .getModel()
+                .getMedia()
+                .get(0));
+
+    String exceptionMessage = String.valueOf(result.getThrowable());
+    extentTestThreadLocal
+        .get()
+        .fail(
+            "<details>"
+                + "<summary>"
+                + "<b>"
+                + "<font color="
+                + "red>"
+                + "Exception Occurred:Click to see"
+                + "</font>"
+                + "</b>"
+                + "</summary>"
+                + exceptionMessage.replace(",", "<br>")
+                + "</details>"
+                + " \n");
+
+    String failureLog = "TEST CASE FAILED";
+    Markup markup = MarkupHelper.createLabel(failureLog, ExtentColor.RED);
+    extentTestThreadLocal.get().fail(markup);
+  }
+
+  @Override
+  public void onTestSkipped(ITestResult result) {
+    extentTestThreadLocal.get().skip(result.getThrowable());
+    extentTestThreadLocal.get().skip("Test Case Skipped");
+  }
+
+  @Override
+  public void onTestFailedButWithinSuccessPercentage(ITestResult result) {}
+
+  @Override
+  public void onStart(ITestContext context) {}
+
+  @Override
+  public void onFinish(ITestContext context) {
+    extent.flush();
+    extentTestThreadLocal.remove();
+  }
+} // End of class ExtentTestNGITestListener
